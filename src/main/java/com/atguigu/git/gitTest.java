b006a17f26ec6bb9cd5beff8c2af6d6ca49b3aec
ruanjiazheng <2403442800@qq.com>
2022-10-08 21:33:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author RuanJiaZheng
+ * @Description
+ * @Date 2022/10/8 21:31
+ * @Version 1.0
+ */
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
