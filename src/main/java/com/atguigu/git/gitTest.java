5b359a1193ff38620772731dd631d1629f394da5
ruanjiazheng <2403442800@qq.com>
2022-10-09 09:02:17 +0800

    master commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -12,6 +12,6 @@ public class gitTest {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
-        System.out.println("crm test");
+        System.out.println("master test");
     }
 }
